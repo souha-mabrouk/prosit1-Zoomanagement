5203a46b7c1d5314fe82583f2d269d6d43020c99
souha-mabrouk <165108580+souha-mabrouk@users.noreply.github.com>
2025-10-03 00:14:24 +0200

    Initial commit: instruction1

---
+++ src/ZooManagement.java
@@ -0,0 +1,14 @@
+public class ZooManagement {
+    static int nbrCages=20;
+    static String zooName="my Zoo";
+
+    ZooManagement zoo=new ZooManagement();
+
+    public static void main(String [] args) {
+        System.out.println( zooName + " comporte " + nbrCages + " cages.");
+    }
+
+}
+ /*public static void main (String [] args) {
+     System.out.println( zooName + "comporte" + nbrCages + "cages.");
+ }*/
---
+++ src/main.java
@@ -0,0 +1,8 @@
+/*public class main {
+
+    ZooManagement zoo=new ZooManagement();
+
+    public static void main(String [] args) {
+        System.out.println( zoo.zooName + "comporte" + zoo.nbrCages + "cages.");
+    }
+}*/
