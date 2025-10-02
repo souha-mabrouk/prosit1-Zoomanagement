b77038fea1c4a3ecadf0bb791ff0b69bc15348bd
souha-mabrouk <165108580+souha-mabrouk@users.noreply.github.com>
2025-10-03 01:40:59 +0200

    Second commit: instruction2

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -11,10 +11,28 @@ public class ZooManagement {
 
         ZooManagement zoo = new ZooManagement();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("donner le nom de zoo");
-        zoo.zooName = scanner.nextLine();
-        System.out.println("donner le nombre des cages");
-        zoo.nbrCages = scanner.nextInt();
+        /*System.out.println("donner le nom de zoo");
+        zoo.zooName = scanner.nextLine();*/
+        do {
+            System.out.println("Donner le nom du zoo :");
+            zoo.zooName = scanner.nextLine(); // supprime les espaces en début/fin
+            if (zoo.zooName.isEmpty()) {
+                System.out.println("Erreur : le nom du zoo ne peut pas être vide !");
+            }
+        } while (zoo.zooName.isEmpty());
+        /*System.out.println("donner le nombre des cages");
+        zoo.nbrCages = scanner.nextInt();*/
+        do {
+            System.out.println("Donner le nombre des cages :");
+            while (!scanner.hasNextInt()) { // vérifier que c'est un entier
+                System.out.println("Erreur : veuillez entrer un ENTIER positif !");
+                scanner.next(); // passer l'entrée invalide
+            }
+            zoo.nbrCages = scanner.nextInt();
+            if (zoo.nbrCages <= 0) {
+                System.out.println("Erreur : le nombre de cages doit être POSITIF !");
+            }
+        } while (zoo.nbrCages <= 0);
         System.out.println(zoo.zooName + " comporte " + zoo.nbrCages + " cages.");
     }
 }
