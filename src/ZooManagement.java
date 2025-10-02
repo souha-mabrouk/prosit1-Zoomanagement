0a957fc5726678d2fb63a89d1453772af18b78f0
souha-mabrouk <165108580+souha-mabrouk@users.noreply.github.com>
2025-10-03 01:33:34 +0200

    Second commit: instruction2

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
+
 public class ZooManagement {
-    static int nbrCages=20;
-    static String zooName="my Zoo";
+    static int nbrCages = 20;
+    static String zooName = "my Zoo";
 
-    ZooManagement zoo=new ZooManagement();
+    //ZooManagement zoo=new ZooManagement();
 
-    public static void main(String [] args) {
-        System.out.println( zooName + " comporte " + nbrCages + " cages.");
+    public static void main(String[] args) {
+        //sSystem.out.println(zooName + " comporte " + nbrCages + " cages.");
+
+        ZooManagement zoo = new ZooManagement();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("donner le nom de zoo");
+        zoo.zooName = scanner.nextLine();
+        System.out.println("donner le nombre des cages");
+        zoo.nbrCages = scanner.nextInt();
+        System.out.println(zoo.zooName + " comporte " + zoo.nbrCages + " cages.");
     }
-
 }
  /*public static void main (String [] args) {
      System.out.println( zooName + "comporte" + nbrCages + "cages.");
